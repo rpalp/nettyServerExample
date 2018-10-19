9fff03214fe66d9de7313edf8e8fd6c695e6f29b
arkan <rpalpmob@gmail.com>
2018-10-19 18:45:15 +0300

    Initial commit

---
+++ src/main/java/alpdev/nettyModifyDiscardServer/Starter.java
@@ -0,0 +1,12 @@
+package alpdev.nettyModifyDiscardServer;
+
+public class Starter {
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        }
+
+        new DiscardServer(port).run();
+    }
+}
